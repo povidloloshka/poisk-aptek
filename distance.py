f63aadacf0e1d7d12f1bb7d374b53ac548840e52
povidloloshka <156359904+povidloloshka@users.noreply.github.com>
2024-03-11 17:25:53 +0300

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,16 @@
+import math
+
+
+def lonlat_distance(a, b):
+    degree_to_meter_factor = 111 * 1000
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    radians_lat = math.radians((a_lat + b_lat) / 2.0)
+    lat_lon_factor = math.cos(radians_lat)
+
+    dx = abs(a_lon - b_lon) * degree_to_meter_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meter_factor * lat_lon_factor
+
+    distance = math.sqrt(dx * dx + dy * dy)
+    return distance
