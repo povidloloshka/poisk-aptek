54fbc5541973623fde1e7fbdb25187a3f08ee5f2
povidloloshka <156359904+povidloloshka@users.noreply.github.com>
2024-03-11 17:08:50 +0300

    Create geo.py

---
+++ geo.py
@@ -0,0 +1,34 @@
+import requests
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def geocode(address):
+    geocoder_api_server = 'http://geocode-maps.yandex.ru/1.x/'
+    geocoder_params = {'apikey': API_KEY, 'geocode': address, 'format': 'json'}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    json_response = response.json()
+    toponyms = json_response['response']['GeoObjectCollection']['featureMember']
+    return toponyms[0]['GeoObject'] if toponyms else None
+
+
+def get_сoordinates(address):
+    toponym = geocode(address)
+    toponym_coord = toponym['Point']['pos']
+    toponym_long, toponym_lat = toponym_coord.split(' ')
+    return float(toponym_long), float(toponym_lat)
+
+
+def get_ll_span(address):
+    toponym = geocode(address)
+    if not toponym:
+        return(None, None)
+    toponym_coord = toponym['Point']['pos']
+    toponym_long, toponym_lat = toponym_coord.split(' ')
+    ll = ','.join([toponym_long, toponym_lat])
+    env = toponym['boundedBy']['Envelope']
+    l, b = env['lowerCorner'].split(' ')
+    r, t = env['upperCorner'].split(' ')
+    dx = abs(float(l) - float(r)) / 2
+    dy = abs(float(t) - float(b)) / 2
+    span = f'{dx},{dy}'
+    return ll, span
